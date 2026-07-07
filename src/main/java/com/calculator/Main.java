1eb7cc118d92b1f930497fe3a22900cd02f4d0e2
IT24102979 <thisulsemitha6@gmail.com>
2026-07-07 13:21:25 +0530

    Innitial commit

---
+++ src/main/java/com/calculator/Main.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+
+import ui.calculatorFrame;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        calculatorFrame frame = new calculatorFrame();
+        frame.Frame();
+
+    }
+}
